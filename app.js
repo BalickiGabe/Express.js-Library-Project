e25737299cfe85fe459c5413d1df512067a6251d
Gabe Balicki <balickigabe@gmail.com>
2025-02-18 10:16:25 -0700

    updated nav functionality in index.js. added about router for about page

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const app = express();
 const bookRouter = require("./routes/bookRouter");
 const authorRouter = require("./routes/authorRouter");
 const indexRouter = require("./routes/indexRouter");
+const aboutRouter = require("./routes/aboutRouter");
 const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
 
-app.set("views", Path2D.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //routes
 app.use("/", indexRouter);
+app.use("/about", aboutRouter);
 app.use("/books", bookRouter);
 app.use("/authors", authorRouter);
+app.use(express.static(assetsPath));
 
 //error handling
 app.use((err, req, res, next) => {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,15 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
 indexRouter.get("/", (req, res) => {
-  res.send("Home page");
+  res.render("index", { links: links, users: users });
 });
 
 module.exports = indexRouter;
